33de2a4e3374a0bc631a95682d5c09e010673d70
TaiJP119 <taijp0119@gmail.com>
2025-05-22 17:33:11 +0800

    change

--- contracts/SimpleContract.sol
+++ contracts/SimpleContract.sol
@@ -4,9 +4,9 @@ pragma solidity  ^0.8.0;
 string public message;  
   
     constructor(string memory initialMessage) {         
-message = initialMessage;  
+        message = initialMessage;  
     }   
     function setMessage(string memory newMessage) public {         
-message = newMessage;  
+        message = newMessage;  
     }  
 }  
